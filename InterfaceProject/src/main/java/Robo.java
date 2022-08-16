14edfa7e74f43d8e047bc7a9f1df9af9433ecafa
Gabriel Albuquerque <gabriel.albuquerque2001@gmail.com>
2022-08-16 08:07:00 -0300

    Commit interface em java

---
+++ InterfaceProject/src/main/java/Robo.java
@@ -0,0 +1,17 @@
+public class Robo implements Controle{
+
+    @Override
+    public void andar() {
+        System.out.println("Andando...");
+    }
+
+    @Override
+    public void virar() {
+        System.out.println("Virando...");
+    }
+
+    @Override
+    public void falar() {
+        System.out.println("Falando...");
+    }
+}
---
+++ InterfaceProject/src/main/java/UsaRobo.java
@@ -0,0 +1,9 @@
+public class UsaRobo extends Robo{
+    public static void main(String[] args) {
+        Robo r1 = new Robo();
+
+        r1.andar();
+        r1.falar();
+        r1.virar();
+    }
+}
